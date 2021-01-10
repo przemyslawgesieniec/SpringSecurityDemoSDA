445620cdf57dddcb53515dff7ddff32cd1d65a33
x_Gesiepr1 <przemyslaw.gesieniec@onespan.com>
2021-01-10 13:00:28 +0100

    initial commit

---
+++ src/main/java/pl/sda/SpringSecuritySda/SpringSecuritySdaApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.SpringSecuritySda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/SpringSecuritySda/SpringSecuritySdaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.SpringSecuritySda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
